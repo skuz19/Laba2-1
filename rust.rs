802469a55b7324645074cfe7519e69092840289b
StanislavKuznetsov <stas.stas-134@mail.ru>
2026-04-23 16:48:07 +0700

    edit rust.rs

--- rust.rs
+++ rust.rs
@@ -1,3 +1,6 @@
+#![allow(non_snake_case)]
+#![allow(unused_mut)]
+
 use std::io;
 
 fn main() {
@@ -16,36 +19,46 @@ fn main() {
 
     let mut total = N;
     let mut empty = N;
-    let mut steps = 0;
+    let mut steps = 1;
 
     println!("\nПошаговый процесс:");
 
-    while empty > 0 {
-        if empty >= M {
-            let groups = empty / M;
-            let used = groups * M;
-
-            // вывод групп
-            for _ in 0..groups {
-                print!("1 ");
-            }
-            steps += 1;
-            println!("-> шаг {}", steps);
-
-            // вывод пустых
-            for _ in 0..used {
-                print!("0 ");
-            }
+    // начальные банки
+    for i in 0..N {
+        print!("1 ");
+        if (i + 1) % M == 0 {
             println!();
-
-            total += groups;
-            empty = empty - used + groups;
-        } else {
-            // остатки
-            steps += 1;
-            println!("1 -> шаг {}", steps);
-            empty -= 1;
         }
+    }
+    println!("\n{} шаг", steps);
+
+    // обмены
+    while empty >= M {
+        let groups = empty / M;
+        let used = groups * M;
+
+        // пустые
+        for i in 0..used {
+            print!("0 ");
+            if (i + 1) % M == 0 {
+                println!();
+            }
+        }
+
+        steps += 1;
+        println!("{} шаг", steps);
+
+        // новые полные
+        for _ in 0..groups {
+            print!("1 ");
+        }
+        println!();
+
+        total += groups;
+        empty = empty - used + groups;
+
+        steps += 1;
+        println!("{} шаг", steps);
     }
 
     println!("\nРезультат:");
